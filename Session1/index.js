ebd21a55857b13a9a07c93084127ac6006fa4556
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2025-01-08 22:12:49 +0530

    session2

--- Session1/index.js
+++ Session1/index.js
@@ -41,20 +41,170 @@ const containerElement = document.getElementById("container");
 console.dir(containerElement)
 
 
-containerElement.addEventListener("click",  () => {
+containerElement.addEventListener("click",  (eventObject) => {
+    console.log(eventObject);
     containerElement.style.background = "green";
     containerElement.textContent = "CLICKED!!!"
 })
     
 
 
-containerElement.addEventListener("mouseover", () => {
+containerElement.addEventListener("mouseover", (event) => {
+    console.log(event);
     containerElement.style.background = "yellow";
     containerElement.textContent = "someone Entered!!!"
 })
 
-containerElement.addEventListener("mouseout", () => {
+containerElement.addEventListener("mouseout", (event) => {
+    console.log(event);
+
     containerElement.style.background = "white";
     containerElement.textContent = "someone exited!!!"
 })
 
+
+// eventObject  -> is nothing the the representation of event 
+
+/**
+ * {
+ *  isTrusted
+: 
+true
+altKey
+: 
+false
+bubbles
+: 
+true
+button
+: 
+0
+buttons
+: 
+0
+cancelBubble
+: 
+false
+cancelable
+: 
+true
+clientX
+: 
+52
+clientY
+: 
+86
+composed
+: 
+true
+ctrlKey
+: 
+false
+currentTarget
+: 
+null
+defaultPrevented
+: 
+false
+detail
+: 
+0
+eventPhase
+: 
+0
+fromElement
+: 
+null
+layerX
+: 
+52
+layerY
+: 
+191
+metaKey
+: 
+false
+movementX
+: 
+0
+movementY
+: 
+0
+offsetX
+: 
+23
+offsetY
+: 
+35
+pageX
+: 
+52
+pageY
+: 
+191
+relatedTarget
+: 
+null
+returnValue
+: 
+true
+screenX
+: 
+47
+screenY
+: 
+-699
+shiftKey
+: 
+false
+sourceCapabilities
+: 
+InputDeviceCapabilities {firesTouchEvents: false}
+srcElement
+: 
+div#container
+target
+: 
+div#container
+timeStamp
+: 
+22683.70000000298
+toElement
+: 
+div#container
+type
+: 
+"mouseover"
+view
+: 
+Window {window: Window, self: Window, document: document, name: '', location: Location, …}
+which
+: 
+0
+x
+: 
+52
+y
+: 
+86
+ * }
+ */
+{}
+
+
+// utkarsh -> OBJ 
+// {
+//     name: "utkarsh",
+//     age: 26,
+//     salary: 1000,
+
+// }
+
+
+// IPL -> OBJ 
+// {
+//     startDate: "feb",
+//     endDate: "march",
+//     winner: "csk",
+//     2ndWinner: ""
+// }
---
+++ Session2/index.js
@@ -0,0 +1,85 @@
+const colorDisplayElement = document.getElementById("target");
+const palletElement = document.getElementById("pallet");
+
+const greenElement = document.getElementById("green");
+const redElement = document.getElementById("red");
+const blueElement = document.getElementById("blue");
+const yellowElement = document.getElementById("yellow");
+
+
+// greenElement.addEventListener("click", () => {
+//     targetElement.style.background = "green"
+//     targetElement.textContent ="Selected Color: green";
+// })
+
+// redElement.addEventListener("click", () => {
+//     targetElement.style.background = "red"
+//     targetElement.textContent = "Selected Color: " + "red";
+// })
+
+// blueElement.addEventListener("click", () => {
+//     targetElement.style.background = "blue"
+//     targetElement.textContent = "Selected Color: " + "blue";
+// })
+
+// yellowElement.addEventListener("click", () => {
+//     targetElement.style.background = "yellow"
+//     targetElement.textContent = "Selected Color: " + "yellow";
+// })
+
+
+// addEventListener -> it takes space in RAM -> 4 space in ram 
+// IS this a good way ?  YES OR NO ? 
+
+
+
+// -------- event delegation ---------------
+
+// palletElement.addEventListener("click", (eventOvb) => {
+//     console.log(eventOvb)
+//     const culpritElement = eventOvb.target;
+//     const color = culpritElement.id;
+//     console.log(culpritElement, color);
+
+//     colorDisplayElement.style.background = color
+//     colorDisplayElement.textContent = "Selected Color: " + color;
+// })
+
+
+// it takes space in RAM -> 1 space in ram 
+
+
+
+
+// FORMS 
+
+
+// HTML - default behaviour of form is to SUBMIT the data to backend and then reload the page
+
+const form = document.getElementById("booking-form");
+
+console.dir(form)
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); // this will prevent the reload of page and giving data to backend
+    console.log("form submitted");
+
+
+    const name = form.elements["fullName"].value;
+    const email = form.elements["email"].value;
+    const doctor = form.elements["doctor"].value;
+    const location = form.elements["location"].value;
+    const date = form.elements["date"].value;
+
+    const bookingDetail = {
+        name: name,
+        email: email,
+        doctor: doctor,
+        location: location,
+        datezazzz: date
+    }
+
+    // VALIDATION 
+
+    console.log(bookingDetail);
+})
